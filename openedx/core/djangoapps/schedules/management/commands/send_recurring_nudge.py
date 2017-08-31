db7f54c1146c6a523adb2d63a0a42504b8615fc1
Gabe Mulley <gabe@edx.org>
2017-08-31 07:34:19 -0400

    move tasks into tasks.py

--- openedx/core/djangoapps/schedules/management/commands/send_recurring_nudge.py
+++ openedx/core/djangoapps/schedules/management/commands/send_recurring_nudge.py
@@ -2,33 +2,20 @@ from __future__ import print_function
 
 import datetime
 import logging
-import pytz
+
 from django.contrib.sites.models import Site
-
-from celery import task
 from django.core.management.base import BaseCommand
-from django.conf import settings
-from django.core.urlresolvers import reverse
-from django.utils.http import urlquote
+import pytz
 
-from openedx.core.djangoapps.schedules.models import Schedule, ScheduleConfig
+from edx_ace.utils.date import serialize
+from openedx.core.djangoapps.schedules.models import ScheduleConfig
+from openedx.core.djangoapps.schedules.tasks import recurring_nudge_schedule_hour
 from openedx.core.djangoapps.site_configuration.models import SiteConfiguration
 
-from edx_ace.message import MessageType
 from edx_ace.recipient_resolver import RecipientResolver
-from edx_ace import ace
-from edx_ace.recipient import Recipient
-from edx_ace.utils.date import serialize, deserialize
 
 
 LOG = logging.getLogger(__name__)
-ROUTING_KEY = getattr(settings, 'ACE_ROUTING_KEY', None)
-
-
-class RecurringNudge(MessageType):
-    def __init__(self, week, *args, **kwargs):
-        super(RecurringNudge, self).__init__(*args, **kwargs)
-        self.name = "recurringnudge_week{}".format(week)
 
 
 class ScheduleStartResolver(RecipientResolver):
@@ -62,76 +49,7 @@ class ScheduleStartResolver(RecipientResolver):
         target_date = self.current_date - datetime.timedelta(days=week * 7)
         for hour in range(24):
             target_hour = target_date + datetime.timedelta(hours=hour)
-            _schedule_hour.apply_async((self.site.id, week, serialize(target_hour), org_list, exclude_orgs, override_recipient_email), retry=False)
-
-
-@task(ignore_result=True, routing_key=ROUTING_KEY)
-def _schedule_hour(site_id, week, target_hour_str, org_list, exclude_orgs=False, override_recipient_email=None):
-    target_hour = deserialize(target_hour_str)
-    msg_type = RecurringNudge(week)
-
-    for (user, language, context) in _schedules_for_hour(target_hour, org_list, exclude_orgs=exclude_orgs):
-        msg = msg_type.personalize(
-            Recipient(
-                user.username,
-                override_recipient_email or user.email,
-            ),
-            language,
-            context,
-        )
-        _schedule_send.apply_async((site_id, msg), retry=False)
-
-
-@task(ignore_result=True, routing_key=ROUTING_KEY)
-def _schedule_send(site_id, msg):
-    site = Site.objects.get(pk=site_id)
-    if not ScheduleConfig.current(site).deliver_recurring_nudge:
-        return
-
-    ace.send(msg)
-
-
-def _schedules_for_hour(target_hour, org_list, exclude_orgs=False):
-    schedules = Schedule.objects.select_related(
-        'enrollment__user__profile',
-        'enrollment__course',
-    ).filter(
-        start__gte=target_hour,
-        start__lt=target_hour + datetime.timedelta(minutes=60),
-        enrollment__is_active=True,
-    )
-
-    if org_list is not None:
-        if exclude_orgs:
-            schedules = schedules.exclude(enrollment__course__org__in=org_list)
-        else:
-            schedules = schedules.filter(enrollment__course__org__in=org_list)
-
-    if "read_replica" in settings.DATABASES:
-        schedules = schedules.using("read_replica")
-
-    for schedule in schedules:
-        enrollment = schedule.enrollment
-        user = enrollment.user
-
-        course_id_str = str(enrollment.course_id)
-        course = enrollment.course
-
-        course_root = reverse('course_root', args=[course_id_str])
-
-        def absolute_url(relative_path):
-            return u'{}{}'.format(settings.LMS_ROOT_URL, urlquote(relative_path))
-
-        template_context = {
-            'student_name': user.profile.name,
-            'course_name': course.display_name,
-            'course_url': absolute_url(course_root),
-
-            # This is used by the bulk email optout policy
-            'course_id': course_id_str,
-        }
-
-        yield (user, course.language, template_context)
+            recurring_nudge_schedule_hour.apply_async((self.site.id, week, serialize(target_hour), org_list, exclude_orgs, override_recipient_email), retry=False)
 
 
 class Command(BaseCommand):
