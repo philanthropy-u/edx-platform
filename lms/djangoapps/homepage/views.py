4a9072937ebff1e84f4659995e6c6575d5e7bd6c
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-09-22 12:23:30 +0000

    Home page added as index page

--- lms/djangoapps/homepage/views.py
+++ lms/djangoapps/homepage/views.py
@@ -2,4 +2,4 @@ from edxmako.shortcuts import render_to_response
 
 
 def home_page(request):
-    return render_to_response("homepage/home_page.html", {})
+    return render_to_response("homepage/homepage.html", {})
