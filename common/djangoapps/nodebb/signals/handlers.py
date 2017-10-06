8ef967cbd38454d48a0a0b787d51ea5432b82d87
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-10-06 11:32:49 +0000

    Renamed the tasks

--- common/djangoapps/nodebb/signals/handlers.py
+++ common/djangoapps/nodebb/signals/handlers.py
@@ -3,7 +3,7 @@ from logging import getLogger
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from nodebb.tasks import create_category_on_nodebb_task, join_group_on_nodebb_task
+from nodebb.tasks import task_create_category_on_nodebb, task_join_group_on_nodebb
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from student.models import ENROLL_STATUS_CHANGE, EnrollStatusChange
 from xmodule.modulestore.django import modulestore
@@ -15,7 +15,7 @@ log = getLogger(__name__)
 def create_category_on_nodebb(sender, instance, created, **kwargs):
     if created:
         # Start a celery task to create a community on node bb
-        create_category_on_nodebb_task.delay(instance.display_name)
+        task_create_category_on_nodebb.delay(instance.display_name)
 
 
 @receiver(ENROLL_STATUS_CHANGE)
@@ -24,4 +24,4 @@ def join_group_on_nodebb(sender, event=None, user=None, **kwargs):  # pylint: di
         course = modulestore().get_course(kwargs.get('course_id'))
 
         # Start a celery task to add a user in the group on node bb
-        join_group_on_nodebb_task.delay(user.username, course.display_name)
+        task_join_group_on_nodebb.delay(user.username, course.display_name)
--- common/djangoapps/nodebb/tasks.py
+++ common/djangoapps/nodebb/tasks.py
@@ -10,7 +10,7 @@ log = logging.getLogger('edx.celery.task')
 
 
 @task(bind=True, default_retry_delay=settings.NODEBB_TASK_DELAY, max_retries=settings.NODEBB_TASK_MAX_TRIES)
-def create_category_on_nodebb_task(self, course_name):
+def task_create_category_on_nodebb(self, course_name):
     status_code, response_body = NodeBBClient().categories.create(name=course_name)
 
     if status_code != 200:
@@ -18,24 +18,21 @@ def create_category_on_nodebb_task(self, course_name):
             self.retry()
         except MaxRetriesExceededError:
             log.error(
-                "Error: Can't create nodebb community for the given course {} due to {}".format(
-                    course_name, response_body
-                )
+                "Error: Can't create nodebb community for the given course %s due to %s" % (course_name, response_body)
             )
     else:
-        log.info('Success: Community created for course {}'.format(course_name))
+        log.info('Success: Community created for course %s' % course_name)
 
 
 @task(bind=True, default_retry_delay=settings.NODEBB_TASK_DELAY, max_retries=settings.NODEBB_TASK_MAX_TRIES)
-def join_group_on_nodebb_task(self, username, course_name):
+def task_join_group_on_nodebb(self, username, course_name):
     status_code, response_body = NodeBBClient().users.join(group_name=course_name, user_name=username)
 
     if status_code != 200:
         try:
             self.retry()
+
         except MaxRetriesExceededError:
-            log.error(
-                'Error: Can not join the group, user ({}, {}) due to {}'.format(course_name, username, response_body)
-            )
+            log.error('Error: Can not join the group, user (%s, %s) due to %s' % (course_name, username, response_body))
     else:
-        log.info('Success: User have joined the group {} successfully'.format(course_name))
+        log.info('Success: User have joined the group %s successfully' % course_name)
