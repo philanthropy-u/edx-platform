ee8f1630cd3b39c6a6b60db19a46eea430916186
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-12-04 07:43:22 +0000

    code reformatting

--- lms/djangoapps/third_party_surveys/admin.py
+++ lms/djangoapps/third_party_surveys/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from lms.djangoapps.third_party_surveys.models import ThirdPartySurvey
-# Register your models here.
 
 admin.site.register(ThirdPartySurvey)
--- lms/djangoapps/third_party_surveys/tasks.py
+++ lms/djangoapps/third_party_surveys/tasks.py
@@ -1,4 +1,3 @@
-
 from surveygizmo import SurveyGizmo
 from lms.djangoapps.third_party_surveys.models import ThirdPartySurvey
 from datetime import date, timedelta
@@ -6,12 +5,13 @@ from celery.task import periodic_task
 from celery.schedules import crontab
 import json
 
+
 @periodic_task(run_every=crontab())
 def get_third_party_surveys():
     survey_client = SurveyGizmo(
         api_version='v4',
-        api_token = "",
-        api_token_secret = ""
+        api_token="",
+        api_token_secret=""
     )
 
     surveys = survey_client.api.survey.list()
@@ -26,9 +26,9 @@ def get_third_party_surveys():
     for survey in surveys['data']:
         print(str(last_requested_date))
         print(str(date.today()))
-        survey_response = survey_client.api.surveyresponse\
-            .filter('datesubmitted','>=', str(last_requested_date))\
-            .filter('datesubmitted','<', str(date.today()))\
+        survey_response = survey_client.api.surveyresponse \
+            .filter('datesubmitted', '>=', str(last_requested_date)) \
+            .filter('datesubmitted', '<', str(date.today())) \
             .list(survey['id'])
 
         print(survey_response['data'])
