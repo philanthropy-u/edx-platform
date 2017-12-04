ccd3d631bde31661f1357ce9934c891b94e558e5
rosheen33 <rosheen.pervaiz@arbisoft.com>
2017-12-04 13:29:41 +0000

    added survey tasks

--- lms/djangoapps/third_party_surveys/models.py
+++ lms/djangoapps/third_party_surveys/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from model_utils.models import TimeStampedModel
 
 
-class ThirdPartySurvey(models.Model):
+class ThirdPartySurvey(TimeStampedModel):
     """
     Model that stores third party surveys
     """
     response = models.TextField()
-    request_date = models.DateField()
+    request_date = models.CharField(max_length=19)
+    user = models.ForeignKey(User, related_name='survey_user')
+    type = models.CharField(max_length=20, null=True, blank=True)
+
+    def __unicode__(self):
+        return "{} | {} | {}".format(self.user, self.type, self.request_date)
--- lms/djangoapps/third_party_surveys/tasks.py
+++ lms/djangoapps/third_party_surveys/tasks.py
@@ -1,37 +1,52 @@
-from surveygizmo import SurveyGizmo
-from lms.djangoapps.third_party_surveys.models import ThirdPartySurvey
-from datetime import date, timedelta
-from celery.task import periodic_task
 from celery.schedules import crontab
-import json
+from celery.task import periodic_task
+from django.conf import settings
+from surveygizmo import SurveyGizmo
+
+from lms.djangoapps.third_party_surveys.models import ThirdPartySurvey
 
 
 @periodic_task(run_every=crontab())
 def get_third_party_surveys():
     survey_client = SurveyGizmo(
         api_version='v4',
-        api_token="",
-        api_token_secret=""
+        api_token=settings.SURVEY_GIZMO_TOKEN,
+        api_token_secret=settings.SURVEY_GIZMO_TOKEN_SECRET
     )
 
     surveys = survey_client.api.survey.list()
-    last_survey = ThirdPartySurvey.objects.last()
-    last_requested_date = date.today() - timedelta(1)
-
-    print(len(surveys['data']))
-
-    if last_survey:
-        last_requested_date = last_survey.request_date
+    try:
+        last_survey = ThirdPartySurvey.objects.latest('request_date')
+    except ThirdPartySurvey.DoesNotExist:
+        last_survey = ''
 
     for survey in surveys['data']:
-        print(str(last_requested_date))
-        print(str(date.today()))
-        survey_response = survey_client.api.surveyresponse \
-            .filter('datesubmitted', '>=', str(last_requested_date)) \
-            .filter('datesubmitted', '<', str(date.today())) \
-            .list(survey['id'])
+        survey_response_filter = survey_client.api.surveyresponse
+        if last_survey:
+            survey_response_filter = survey_response_filter.resultsperpage('500').filter(
+                'datesubmitted', '>', last_survey.request_date
+            )
 
-        print(survey_response['data'])
+        survey_responses = survey_response_filter.list(survey['id'])
 
-        third_party_survey = ThirdPartySurvey(response=json.dumps(survey_response['data']), request_date=date.today())
+        # Pagination
+        total_pages = survey_responses['total_pages']
+
+        for page in range(survey_responses['page'] + 1, total_pages + 1):
+            survey_page_responses = survey_response_filter.page(page).list(survey['id'])
+            save_responses(survey_page_responses['data'])
+
+        save_responses(survey_responses['data'])
+
+
+def save_responses(survey_responses):
+    for response in survey_responses:
+        if not response.get('[url("sguid")]'):
+            continue
+
+        third_party_survey = ThirdPartySurvey(
+            response=response,
+            user_id=response['[url("sguid")]'],
+            request_date=response['datesubmitted']
+        )
         third_party_survey.save()
