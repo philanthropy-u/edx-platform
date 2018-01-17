cb822064844c74028bf6c8d46db868468008f732
Muhammad Nadee <muhammad.nadeem@arbisoft.com>
2018-01-17 13:21:44 +0500

    Updates for LP-584

--- lms/djangoapps/onboarding/views.py
+++ lms/djangoapps/onboarding/views.py
@@ -351,7 +351,7 @@ def update_account_settings(request):
         )
 
     return render(
-        request, 'onboarding/registration_update.html',
+        request, 'myaccount/registration_update.html',
         {'form': form, 'org_url': reverse('get_user_organizations')}
     )
 
