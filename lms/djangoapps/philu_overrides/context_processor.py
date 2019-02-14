3bdd920a865afc477d72fd6e88e0b87454956121
ahmed-faraz46 <ahmed.faraz@arbisoft.com>
2019-02-14 17:09:50 +0500

    moved quires inside oef sanity

--- lms/djangoapps/philu_overrides/context_processor.py
+++ lms/djangoapps/philu_overrides/context_processor.py
@@ -19,6 +19,7 @@ def get_global_alert_messages(request):
     overlay_message = None
     oef_prompt = None
 
+    # metric_update_prompt will tell us if user is responsible for some organization
     metric_update_prompt = get_org_metric_update_prompt(request.user)
     show_org_detail_prompt = metric_update_prompt and is_org_detail_prompt_available(metric_update_prompt)
 
@@ -32,12 +33,15 @@ def get_global_alert_messages(request):
                 "alert": ACTIVATION_ERROR
             })
 
-    if '/oef/dashboard' in request.path and show_oef_prompt:
-        alert_messages.append({
-            "type": ACTIVATION_ALERT_TYPE,
-            "alert": ORG_OEF_UPDATE_ALERT
-        })
-        oef_prompt = True
+    if '/oef/dashboard' in request.path:
+        oef_update_prompt = get_org_oef_update_prompt(request.user)
+        show_oef_prompt = oef_update_prompt and is_org_oef_prompt_available(oef_update_prompt)
+        if show_oef_prompt:
+            alert_messages.append({
+                "type": ACTIVATION_ALERT_TYPE,
+                "alert": ORG_OEF_UPDATE_ALERT
+            })
+            oef_prompt = True
 
     elif '/organization/details/' in request.path and show_org_detail_prompt:
         alert_messages.append({
