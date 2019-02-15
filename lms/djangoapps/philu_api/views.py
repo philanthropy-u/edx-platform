2d350af19b387879c0d32433954881c752e456a4
RanaWaqar7550 <rana.waqar@arbisoft.com>
2019-02-15 12:29:28 +0500

    add a method condition

--- lms/djangoapps/philu_api/views.py
+++ lms/djangoapps/philu_api/views.py
@@ -199,7 +199,7 @@ def download_pdf_file(request):
     """ Download pdf file (Worksheet) instead of opening in the browser """
     query_string = request.META.get('QUERY_STRING')
     page_url = query_string.split('page_url=')[-1]
-    if page_url:
+    if page_url and request.GET:
         filename = page_url.split("/")[-1]
         filename = filename.replace(" ", "_")
         result = urllib.urlopen(page_url)
