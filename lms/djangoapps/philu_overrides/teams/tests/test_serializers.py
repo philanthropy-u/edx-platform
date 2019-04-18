10b2975f9e61d9e345f2935bc24da3186d59d373
usman.khan <usman@A00526.arbisoft.com>
2019-04-18 19:15:03 +0500

    add formatter and optimize code

--- lms/djangoapps/philu_overrides/teams/tests/test_serializers.py
+++ lms/djangoapps/philu_overrides/teams/tests/test_serializers.py
@@ -4,11 +4,21 @@ from xmodule.modulestore import ModuleStoreEnum
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from lms.djangoapps.philu_overrides.teams.serializers import CustomCourseTeamCreationSerializer
 
+VALID_COUNTRY = 'LA'
+VALID_LANGUAGE = 'kl'
+INVALID_COUNTRY = 'Test Country'
+INVALID_LANGUAGE = 'Test Language'
+
+REQUIRED_ERROR_MSG = {'COUNTRY': ['Country field is required'], 'LANGUAGE': ['Language field is required']}
+INVALID_COUNTRY_CODE = '{country_code} is not a valid country code'
+INVALID_LANGUAGE_CODE = '{language_code} is not a valid language code'
+
 
 class CreateTeamsSerializerTestCase(ModuleStoreTestCase):
     """
         Tests for the create team serializer.
     """
+
     def setUp(self):
         super(CreateTeamsSerializerTestCase, self).setUp()
         org = 'edX'
@@ -36,58 +46,59 @@ class CreateTeamsSerializerTestCase(ModuleStoreTestCase):
         )
 
     def test_create_team_empty_language_field_case(self):
-        self.team.country = 'LA'
+        self.team.country = VALID_COUNTRY
         data = self.team.__dict__
-        expected_result = {'language': ['Language field is required']}
+        expected_result = {'language': REQUIRED_ERROR_MSG['LANGUAGE']}
         serialized_data = CustomCourseTeamCreationSerializer(data=data)
         serialized_data.is_valid()
         self.assertEqual(serialized_data.errors, expected_result)
 
     def test_create_team_invalid_language_code_case(self):
-        self.team.country = 'LA'
-        self.team.language = 'klkl'
+        self.team.country = VALID_COUNTRY
+        self.team.language = INVALID_LANGUAGE
         data = self.team.__dict__
-        expected_result = {'language': ['klkl is not a valid language code']}
+        expected_result = {'language': [INVALID_LANGUAGE_CODE.format(language_code=self.team.language)]}
         serialized_data = CustomCourseTeamCreationSerializer(data=data)
         serialized_data.is_valid()
         self.assertEqual(serialized_data.errors, expected_result)
 
     def test_create_team_empty_country_field_case(self):
-        self.team.language = 'kl'
+        self.team.language = VALID_LANGUAGE
         data = self.team.__dict__
-        expected_result = {'country': ['Country field is required']}
+        expected_result = {'country': REQUIRED_ERROR_MSG['COUNTRY']}
         serialized_data = CustomCourseTeamCreationSerializer(data=data)
         serialized_data.is_valid()
         self.assertEqual(serialized_data.errors, expected_result)
 
     def test_create_team_invalid_country_code_case(self):
-        self.team.country = 'LALA'
-        self.team.language = 'kl'
+        self.team.country = INVALID_COUNTRY
+        self.team.language = VALID_LANGUAGE
         data = self.team.__dict__
-        expected_result = {'country': ['LALA is not a valid country code']}
+        expected_result = {'country': [INVALID_COUNTRY_CODE.format(country_code=self.team.country)]}
         serialized_data = CustomCourseTeamCreationSerializer(data=data)
         serialized_data.is_valid()
         self.assertEqual(serialized_data.errors, expected_result)
 
     def test_create_team_both_empty_fields_case(self):
         data = self.team.__dict__
-        expected_result = {'country': ['Country field is required'], 'language': ['Language field is required']}
+        expected_result = {'country': REQUIRED_ERROR_MSG['COUNTRY'], 'language': REQUIRED_ERROR_MSG['LANGUAGE']}
         serialized_data = CustomCourseTeamCreationSerializer(data=data)
         serialized_data.is_valid()
         self.assertEqual(serialized_data.errors, expected_result)
 
     def test_create_team_both_invalid_code_case(self):
-        self.team.country = 'LALA'
-        self.team.language = 'klkl'
+        self.team.country = INVALID_COUNTRY
+        self.team.language = INVALID_LANGUAGE
         data = self.team.__dict__
-        expected_result = {'country': ['LALA is not a valid country code'], 'language': ['klkl is not a valid language code']}
+        expected_result = {'country': [INVALID_COUNTRY_CODE.format(country_code=self.team.country)],
+                           'language': [INVALID_LANGUAGE_CODE.format(language_code=self.team.language)]}
         serialized_data = CustomCourseTeamCreationSerializer(data=data)
         serialized_data.is_valid()
         self.assertEqual(serialized_data.errors, expected_result)
 
     def test_create_team_valid_code_case(self):
-        self.team.country = 'LA'
-        self.team.language = 'kl'
+        self.team.country = VALID_COUNTRY
+        self.team.language = VALID_LANGUAGE
         data = self.team.__dict__
         expected_result = {}
         serialized_data = CustomCourseTeamCreationSerializer(data=data)
