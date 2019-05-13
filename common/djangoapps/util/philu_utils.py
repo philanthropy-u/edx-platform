d34197473dd7df760d1489d19942291daa7be5e8
Saad Waqar <saad.waqar@arbisoft.com>
2019-05-13 15:44:29 +0500

    implemented review

--- common/djangoapps/util/philu_utils.py
+++ common/djangoapps/util/philu_utils.py
@@ -1,6 +1,10 @@
-
-
 def extract_utm_params(input_dict):
+    """
+    This method returns a subset of the input dictionary that only contains the utm params found
+    in the input_dict
+    :param input_dict: a dictionary that may or may not contain utm parameters
+    :return: a dictionary containing only utm_params found in the utm_keys
+    """
     if not input_dict:
         return dict()
 
@@ -12,4 +16,4 @@ def extract_utm_params(input_dict):
         'utm_term'
     ]
 
-    return {i: v for i, v in input_dict.items() if i in utm_keys}
+    return {key: value for key, value in input_dict.items() if key in utm_keys}
