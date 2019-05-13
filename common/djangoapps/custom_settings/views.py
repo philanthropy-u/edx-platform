872e88cbc0d19039f59ebb7b473e65bbf8ccf066
Imran Bashir <imran.bashir@arbisoft.com>
2019-05-13 16:39:02 +0500

    refactor the code

--- common/djangoapps/custom_settings/views.py
+++ common/djangoapps/custom_settings/views.py
@@ -1,7 +1,5 @@
 import json
 import logging
-import pytz
-from datetime import datetime
 
 import django.utils
 from django.core.exceptions import ObjectDoesNotExist
@@ -13,14 +11,13 @@ from opaque_keys.edx.keys import CourseKey
 
 from cms.djangoapps.contentstore.views.course import get_course_and_check_access
 from edxmako.shortcuts import render_to_response
-from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from util.json_request import JsonResponse, expect_json
 from util.views import require_global_staff
 from xmodule.modulestore.django import modulestore
 from .models import CustomSettings
+from .helpers import get_course_open_date_from_settings, validate_course_open_date
 
 log = logging.getLogger(__name__)
-DATE_FORMAT = "%m/%d/%Y"
 
 
 @require_global_staff
@@ -55,8 +52,7 @@ def course_custom_settings(request, course_key_string):
                     'show_grades': settings.show_grades,
                     'tags': settings.tags,
                     'seo_tags': "" if not settings.seo_tags else json.loads(settings.seo_tags),
-                    'course_open_date': ""
-                    if not settings.course_open_date else settings.course_open_date.strftime(DATE_FORMAT),
+                    'course_open_date': get_course_open_date_from_settings(settings),
                     'auto_enroll': settings.auto_enroll
                 },
                 'custom_settings_url': reverse('custom_settings', kwargs={'course_key_string': unicode(course_key)}),
@@ -77,31 +73,3 @@ def course_custom_settings(request, course_key_string):
 
         else:
             return HttpResponseNotAllowed("Bad Request", content_type="text/plain")
-
-
-def validate_course_open_date(settings, course_open_date):
-
-    """
-    this method compares course start date and course open date
-    and make sure that course open date is greater than start date
-    :param settings:
-    :param course_open_date:
-    :return:
-        validated course open date or none
-    """
-
-    if course_open_date:
-        if isinstance(datetime.strptime(course_open_date, DATE_FORMAT), datetime):
-            course_open_date = datetime.strptime(course_open_date, DATE_FORMAT)
-            utc = pytz.UTC
-            course_open_date = course_open_date.replace(tzinfo=utc)
-            course = CourseOverview.objects.get(id=settings.id)
-
-            if course.end < course_open_date or course_open_date < course.start:
-                raise ValueError('invalid date object', course_open_date)
-
-        else:
-            raise ValueError('invalid date object', course_open_date)
-    else:
-        course_open_date = None
-    return course_open_date
--- lms/djangoapps/philu_overrides/helpers.py
+++ lms/djangoapps/philu_overrides/helpers.py
@@ -65,29 +65,12 @@ def get_course_next_classes(request, course):
     """
 
     # imports to avoid circular dependencies
-    from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
     from lms.djangoapps.courseware.access import _can_enroll_courselike
     from lms.djangoapps.courseware.views.views import registered_for_course
     from student.models import CourseEnrollment
     from opaque_keys.edx.locations import SlashSeparatedCourseKey
-    from course_action_state.models import CourseRerunState
-    from openedx.features.course_card.helpers import get_course_open_date
 
-    courses = []
-
-    current_time = datetime.utcnow().replace(tzinfo=utc)
-    course_rerun_states = [crs.course_key for crs in CourseRerunState.objects.filter(
-        source_course_key=course.id, action="rerun", state="succeeded")] + [course.id]
-
-    course_rerun_objects = CourseOverview.objects.select_related('image_set').filter(
-        id__in=course_rerun_states).order_by('start')
-
-    for course_run in course_rerun_objects:
-
-        course_open_date = get_course_open_date(course_run)
-        if course_open_date >= current_time:
-            course_run.course_open_date = course_open_date
-            courses.append(course_run)
+    courses = get_all_reruns_of_a_course(course)
 
     course_next_classes = []
 
@@ -126,6 +109,30 @@ def get_course_next_classes(request, course):
     return course_next_classes
 
 
+def get_all_reruns_of_a_course(course):
+    """
+    :param course:
+    :return reruns:
+    """
+    from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+    from course_action_state.models import CourseRerunState
+    from openedx.features.course_card.helpers import get_course_open_date
+    courses = []
+    current_time = datetime.utcnow().replace(tzinfo=utc)
+    course_rerun_states = [crs.course_key for crs in CourseRerunState.objects.filter(
+        source_course_key=course.id, action="rerun", state="succeeded")] + [course.id]
+    course_rerun_objects = CourseOverview.objects.select_related('image_set').filter(
+        id__in=course_rerun_states).order_by('start')
+    for course_run in course_rerun_objects:
+
+        course_open_date = get_course_open_date(course_run)
+        if course_open_date >= current_time:
+            course_run.course_open_date = course_open_date
+            courses.append(course_run)
+
+    return courses
+
+
 def get_user_current_enrolled_class(request, course):
     """
     Method to get an ongoing user enrolled course. A course that meets the following criteria
@@ -144,6 +151,7 @@ def get_user_current_enrolled_class(request, course):
         source_course_key=course.id, action="rerun", state="succeeded")] + [course.id]
     current_time = datetime.utcnow().replace(tzinfo=utc)
     current_class = get_course_current_class(all_course_reruns, current_time)
+    course_open_date = current_class.course_open_date
 
     current_enrolled_class = False
     if current_class:
@@ -153,6 +161,7 @@ def get_user_current_enrolled_class(request, course):
     if current_enrolled_class:
         course_key = SlashSeparatedCourseKey.from_deprecated_string(current_class.id.__str__())
         current_class = get_course_by_id(course_key)
+        current_class.course_open_date = course_open_date
         first_chapter_url, first_section = get_course_related_keys(request, current_class)
         current_enrolled_class_target = reverse('courseware_section',
                                                 args=[current_class.id.to_deprecated_string(),
