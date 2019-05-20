f1f3694e74feb9c02ea3ab4b75a8fa4f676e8655
ahmed-faraz46 <ahmed.faraz@arbisoft.com>
2019-05-20 12:38:51 +0500

    add create team form

--- openedx/features/teams/views.py
+++ openedx/features/teams/views.py
@@ -1,9 +1,12 @@
 from django.http import Http404
 from django.shortcuts import render_to_response
-from lms.djangoapps.teams.models import CourseTeam
+from django_countries import countries
+from django.conf import settings
+from django.utils.translation import ugettext as _
 
 from opaque_keys.edx.keys import CourseKey
 from courseware.courses import get_course_with_access, has_access
+from lms.djangoapps.teams.models import CourseTeam
 from lms.djangoapps.teams import is_feature_enabled
 from student.models import CourseEnrollment, CourseAccessRole
 from lms.djangoapps.teams.serializers import (
@@ -15,6 +18,30 @@ from lms.djangoapps.teams.serializers import (
 )
 
 from .helpers import serialize
+
+
+def browse_teams(request, course_id):
+    course_key = CourseKey.from_string(course_id)
+    course = get_course_with_access(request.user, "load", course_key)
+
+    context = {
+        'course': course,
+    }
+
+    return render_to_response("teams/browse_teams.html", context)
+
+
+def create_team(request, course_id, region):
+    course_key = CourseKey.from_string(course_id)
+    course = get_course_with_access(request.user, "load", course_key)
+
+    context = {
+        'course': course,
+        'countries': list(countries),
+        'languages': [[lang[0], _(lang[1])] for lang in settings.ALL_LANGUAGES]
+    }
+
+    return render_to_response("teams/create_team.html", context)
 
 
 def my_team(request, course_id):
@@ -69,15 +96,4 @@ def get_alphabetical_topics(course_module):
         list: a list of sorted team topics
     """
     return sorted(course_module.teams_topics, key=lambda t: t['name'].lower())
-
-
-def browse_teams(request, course_id):
-    course_key = CourseKey.from_string(course_id)
-    course = get_course_with_access(request.user, "load", course_key)
-
-    context = {
-        'course': course,
-    }
-
-    return render_to_response("teams/browse_teams.html", context)
 
