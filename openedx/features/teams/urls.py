8201d16ccb95f48c9d12362006f25ffe7ca9e316
ahmed-faraz46 <ahmed.faraz@arbisoft.com>
2019-05-20 12:38:51 +0500

    add create team form

--- openedx/features/teams/urls.py
+++ openedx/features/teams/urls.py
@@ -3,10 +3,12 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from .views import my_team, browse_teams
+from .views import browse_teams, create_team, my_team
 
 urlpatterns = patterns(
     'openedx.features.teams.views',
+
+    url(r"^browse_teams/$", login_required(browse_teams), name="browse_teams"),
+    url(r"^(?P<region>[a-z0-9]+)/create/$", login_required(create_team), name="create_team"),
     url(r"^my_team/$", login_required(my_team), name="my_team"),
-    url(r"^browse_teams/$", login_required(browse_teams), name="browse_teams")
 )
