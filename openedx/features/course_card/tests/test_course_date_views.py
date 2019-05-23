a9aab5b57c0d8a228ba5fd0eab886d7d29d96d4d
usman.khan <usman@A00526.arbisoft.com>
2019-05-23 12:53:26 +0500

    add tests for course open date

--- openedx/features/course_card/tests/test_course_date_views.py
+++ openedx/features/course_card/tests/test_course_date_views.py
@@ -14,6 +14,7 @@ from openedx.core.djangoapps.theming.models import SiteTheme
 from ..models import CourseCard
 from .helpers import set_course_dates
 
+TEST_COURSE_OPEN_DATE = datetime.utcnow()+timedelta(days=1)
 
 class CourseCardBaseClass(ModuleStoreTestCase):
 
@@ -24,6 +25,7 @@ class CourseCardBaseClass(ModuleStoreTestCase):
         course_number_f = 'CS101'
         course_run = '2015_Q1'
         display_name_f = 'test course 1'
+        self.date_time_format = '%b %-d, %Y'
 
         self.course = CourseFactory.create(org=org, number=course_number_f, run=course_run,
                                            display_name=display_name_f,
@@ -32,6 +34,7 @@ class CourseCardBaseClass(ModuleStoreTestCase):
                                            end=datetime.utcnow() + timedelta(days=30))
 
         self.course.save()
+        set_course_dates(self.course, -30, -10, -5, 30)
         CourseCard(course_id=self.course.id, course_name=self.course.display_name, is_enabled=True).save()
 
     @classmethod
@@ -46,22 +49,19 @@ class CourseCardBaseClass(ModuleStoreTestCase):
 class CourseCardViewBaseClass(CourseCardBaseClass):
 
     def test_catalog_course_date(self):
-        date_time_format = '%b %-d, %Y'
-        set_course_dates(self.course, -30, -10, -5, 30)
         save_course_custom_settings(self.course.id)
         response = self.client.get(reverse('courses'))
         self.assertEqual(pq(response.content)("div.course-date > span")[2].text.strip(),
-                         datetime.utcnow().strftime(date_time_format))
+                         TEST_COURSE_OPEN_DATE.strftime(self.date_time_format))
 
     def test_about_course_date(self):
-        date_time_format = '%b %-d, %Y'
-        set_course_dates(self.course, -30, -10, -5, 30)
         save_course_custom_settings(self.course.id)
         response = self.client.get(reverse('about_course', args=[self.course.id]))
-        self.assertEqual(pq(response.content)("p.start-date")[0].text, datetime.utcnow().strftime(date_time_format))
+        self.assertEqual(pq(response.content)("p.start-date")[0].text,
+                         TEST_COURSE_OPEN_DATE.strftime(self.date_time_format))
 
 
 def save_course_custom_settings(course_key_string):
-    course_settings = CustomSettings(id=course_key_string, course_short_id=1, course_open_date=datetime.utcnow())
+    course_settings = CustomSettings(id=course_key_string, course_short_id=1, course_open_date=TEST_COURSE_OPEN_DATE)
     course_settings.save()
     return course_settings
