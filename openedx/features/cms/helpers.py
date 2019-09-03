809912ad1bb7c36df7399a84d9ad542e35ad3ecd
Saad Waqar <saad.waqar@arbisoft.com>
2019-09-03 18:05:25 +0500

    fixed comma turning string to tuple

--- openedx/features/cms/helpers.py
+++ openedx/features/cms/helpers.py
@@ -256,7 +256,7 @@ def update_course_re_run_details(course_re_run_details):
             error_message = 'This course does not have end date',
 
         if not source_course.enrollment_start:
-            error_message = 'This course does not have enrollment start date',
+            error_message = 'This course does not have enrollment start date'
 
         if not source_course.enrollment_end:
             error_message = 'This course does not have enrollment end date'
