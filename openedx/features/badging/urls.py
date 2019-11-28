e766d7669d4c854719e7569502b836169847e894
Imran Bashir <imran.bashir@arbisoft.com>
2019-11-28 17:53:35 +0500

    create route for trophy case

--- openedx/features/badging/urls.py
+++ openedx/features/badging/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from .views import trophycase
+from .views import trophy_case
 
 urlpatterns = [
-    url(r'^trophycase/$', trophycase, name='trophycase'),
+    url(r'^trophycase/$', trophy_case, name='trophycase'),
 ]
--- openedx/features/badging/views.py
+++ openedx/features/badging/views.py
@@ -1,23 +1,37 @@
 from django.views.decorators.http import require_GET
-from util.json_request import JsonResponse
-
-from student.models import CourseEnrollment
+from django.http import Http404
+# from util.json_request import JsonResponse
+from edxmako.shortcuts import render_to_response
+from student.models import CourseEnrollment, AnonymousUserId
 
 from .helpers import populate_trophycase
 from .models import UserBadge
 
 
 @require_GET
-def trophycase(request):
+def trophy_case(request):
     # Get course id and course name of courses user is enrolled in
-    enrolled_courses_data = CourseEnrollment.enrollments_for_user(request.user).order_by(
-        'course__display_name').values_list('course_id', 'course__display_name')
 
-    # list of badges earned by user
-    earned_user_badges = list(
-        UserBadge.objects.filter(user=request.user)
+    # try:
+    #     user = AnonymousUserId.objects.get(anonymous_user_id=uuid).user
+    # except AnonymousUserId.DoesNotExist:
+    #     raise Http404()
+    #
+    # enrolled_courses_data = CourseEnrollment.enrollments_for_user(user).order_by(
+    #     'course__display_name').values_list('course_id', 'course__display_name')
+    #
+    # # list of badges earned by user
+    # earned_user_badges = list(
+    #     UserBadge.objects.filter(user=user)
+    # )
+    #
+    # trophycase_dict = populate_trophycase(enrolled_courses_data, earned_user_badges)
+
+    # return JsonResponse(trophycase_dict)
+
+    return render_to_response(
+        "features/badging/trophy_case.html",
+        {
+            'course_badges': []
+        }
     )
-
-    trophycase_dict = populate_trophycase(enrolled_courses_data, earned_user_badges)
-
-    return JsonResponse(trophycase_dict)
