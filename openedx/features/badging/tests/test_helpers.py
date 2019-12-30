f59395bae9102959309a0590a1ad926944ad7287
Zahid <zahid.ali@arbisoft.com>
2019-12-30 18:27:33 +0500

    Adding documentation for unit tests

--- openedx/features/badging/tests/test_helpers.py
+++ openedx/features/badging/tests/test_helpers.py
@@ -30,15 +30,28 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
 
 
     def test_populate_trophycase_with_empty_course_list(self):
+        """
+        For empty course list, trophy case should not return any data except empty dictionary
+        :return: None
+        """
         trophycase_dict = badge_helpers.populate_trophycase(user=self.user, courses=list(), earned_badges=mock.ANY)
         self.assertEqual(trophycase_dict, dict())
 
     def test_populate_trophycase_with_courses_none(self):
+        """
+        If course list is None, trophy case should throw error
+        :return: None
+        """
         with self.assertRaises(TypeError):
             badge_helpers.populate_trophycase(user=self.user, courses=None, earned_badges=mock.ANY)
 
     @mock.patch('openedx.features.badging.helpers.get_course_badges')
     def test_populate_trophycase_successful_and_ordered_by_course_name(self, mock_get_course_badges):
+        """
+        For a list of courses, trophy case should return data sorted by course name
+        :param mock_get_course_badges: mock return value for course badges
+        :return: None
+        """
         mock_get_course_badges.return_value = {"badges": 'mocked badges'}
 
         course2 = CourseFactory(org="test2", number="123", run="1", display_name="XYZ")
@@ -69,7 +82,13 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
         self.assertEqual(expected_return_value['test2/123/1'], course_detail[1])
 
     @mock.patch('openedx.features.badging.helpers.Badge.objects.all')
-    def test_course_badges_with_empty_badge_queryset(self, mock_badge_objects_all):
+    def test_get_course_badges_with_empty_badge_queryset(self, mock_badge_objects_all):
+        """
+        Testing optional parameter badge_queryset. If it is None or not passed to function, it
+        should be created from inside
+        :param mock_badge_objects_all: mocking query set & generating fake exception, just to assert on it
+        :return: None
+        """
         message = 'Raising fake exception, if badge_queryset is none'
         mock_badge_objects_all.side_effect = Exception(message)
 
@@ -83,10 +102,18 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
     @mock.patch('openedx.features.badging.helpers.filter_earned_badge_by_joined_team')
     @mock.patch('openedx.features.badging.helpers.is_teams_feature_enabled')
     @mock.patch('openedx.features.badging.helpers.get_course_by_id')
-    def test_course_badges_successfully(self, mock_get_course_by_id,
+    def test_get_course_badges_successfully(self, mock_get_course_by_id,
                                         mock_is_teams_feature_enabled,
                                         mock_filter_earned_badge_by_joined_team,
                                         mock_add_badge_earned_date):
+        """
+        Create 1 course, 3 badges (1 team, 2 conversationalist), none of the badges are earned, to test success case
+        :param mock_get_course_by_id: mock course id, because it is irrelevant here
+        :param mock_is_teams_feature_enabled: mock and return False to disable teams feature
+        :param mock_filter_earned_badge_by_joined_team: mock such that user has no joined team and no earned badge
+        :param mock_add_badge_earned_date: mock and return nothing, since there is no earned badge
+        :return: None
+        """
         mock_get_course_by_id.return_value = mock.ANY
         mock_is_teams_feature_enabled.return_value = False
         mock_filter_earned_badge_by_joined_team.return_value = False, list()
@@ -100,11 +127,11 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
         expected_result = {
             'badges': {
                 'conversationalist': [{
-                    'description': None, 'image': badge2.image, 'threshold': 2L,
+                    'description': None, 'image': badge2.image, 'threshold': 2,
                     'date_created': badge2.date_created, 'type': u'conversationalist',
                     u'id': badge2.id, 'name': badge2.name
                 }, {
-                    'description': None, 'image': badge3.image, 'threshold': 5L,
+                    'description': None, 'image': badge3.image, 'threshold': 5,
                     'date_created': badge3.date_created, 'type': u'conversationalist',
                     u'id': badge3.id, 'name': badge3.name
                 }]
@@ -125,10 +152,19 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
     @mock.patch('openedx.features.badging.helpers.filter_earned_badge_by_joined_team')
     @mock.patch('openedx.features.badging.helpers.is_teams_feature_enabled')
     @mock.patch('openedx.features.badging.helpers.get_course_by_id')
-    def test_course_badges_user_not_joined_any_course_team(self, mock_get_course_by_id,
+    def test_get_course_badges_user_not_joined_any_course_team(self, mock_get_course_by_id,
                                                            mock_is_teams_feature_enabled,
                                                            mock_filter_earned_badge_by_joined_team,
                                                            mock_add_badge_earned_date):
+        """
+        Create 1 course, 1 conversationalist badge, to test course badges, when team feature is enabled but user
+        has not joined any team in a course
+        :param mock_get_course_by_id: mock course id, because it is irrelevant here
+        :param mock_is_teams_feature_enabled: mock and return True to enable teams feature
+        :param mock_filter_earned_badge_by_joined_team: mock such that user has no joined team and no earned badge
+        :param mock_add_badge_earned_date: mock and return nothing, since there is no earned badge
+        :return: None
+        """
         mock_get_course_by_id.return_value = mock.ANY
         mock_is_teams_feature_enabled.return_value = True
         mock_filter_earned_badge_by_joined_team.return_value = False, list()
@@ -140,7 +176,7 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
         expected_result = {
             'badges': {
                 'conversationalist': [{
-                    'description': None, 'image': badge.image, 'threshold': 2L,
+                    'description': None, 'image': badge.image, 'threshold': 2,
                     'date_created': badge.date_created, 'type': u'conversationalist',
                     u'id': badge.id, 'name': badge.name
                 }],
@@ -151,21 +187,35 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
 
         self.assertEqual(expected_result, badges)
 
-    def test_badge_earned_date_no_course_badges(self):
+    def test_add_badge_earned_date_no_course_badges(self):
+        """
+        Assert that exception is raised when adding badge earned date, if provided course badges are None
+        :return: None
+        """
         with self.assertRaises(TypeError):
             badge_helpers.add_badge_earned_date(self.course1.id, course_badges=None, earned_badges=mock.ANY)
 
-    def test_badge_earned_date_no_earned_badges(self):
-        badge = BadgeFactory(type=self.type_conversationalist, threshold=2)
+    def test_add_badge_earned_date_no_earned_badges(self):
+        """
+        Assert that exception is raised when adding badge earned date, if no earned badge exists for
+        provided course badges
+        :return: None
+        """
+        badge = BadgeFactory(threshold=2)
         course_badges = [badge, ]
         with self.assertRaises(TypeError):
             badge_helpers.add_badge_earned_date(self.course1.id, course_badges, earned_badges=None)
 
-    def test_badge_earned_date_course_badges_subset_of_earned_badges(self):
+    def test_add_badge_earned_date_earned_badges_in_different_courses(self):
+        """
+        Create 2 badges, 2 courses and assign one badge per course respectively. Assert that earned date will
+        be added to only that badge whose course id will match with course id provided (in param)
+        :return: None
+        """
         course2 = CourseFactory(org="test2", number="123", run="1", display_name="XYZ")
 
-        badge1 = BadgeFactory(type=self.type_conversationalist, threshold=2)
-        badge2 = BadgeFactory(type=self.type_conversationalist, threshold=5)
+        badge1 = BadgeFactory(threshold=2)
+        badge2 = BadgeFactory(threshold=5)
 
         user_badge_1 = UserBadgeFactory(user=self.user, course_id=self.course1.id, badge=badge1)
         user_badge_2 = UserBadgeFactory(user=self.user, course_id=course2.id, badge=badge2)
@@ -182,8 +232,14 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
 
     @factory.django.mute_signals(signals.pre_save, signals.post_save)
     def test_filter_earned_badge_by_joined_team(self):
-        badge1 = BadgeFactory(type=self.type_conversationalist, threshold=2)
-        badge2 = BadgeFactory(type=self.type_conversationalist, threshold=2)
+        """
+        Create team in course and add user in it. Create 2 badges, both earned by user in a course but in two
+        different teams, one of which user is currently part of. Assert that currently joined team exists and
+        user has earned right number of right badges in that team
+        :return: None
+        """
+        badge1 = BadgeFactory(threshold=2)
+        badge2 = BadgeFactory(threshold=2)
         user_badge1 = UserBadgeFactory(user=self.user, course_id=self.course1.id, badge=badge1, community_id=100)
         user_badge2 = UserBadgeFactory(user=self.user, course_id=self.course1.id, badge=badge2, community_id=101)
 
@@ -201,6 +257,11 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
 
     @factory.django.mute_signals(signals.pre_save, signals.post_save)
     def test_filter_earned_badge_by_joined_team_invalid_room_id(self):
+        """
+        For a course, in which user is part of a team, which has invalid room it, Assert that exception is raised
+        due to invalid room id
+        :return: None
+        """
         course_team_from_factory = CourseTeamFactory(course_id=self.course1.id, team_id='team1')
         CourseTeamMembershipFactory(user=self.user, team=course_team_from_factory)
 
@@ -208,8 +269,13 @@ class BadgeHelperTestCases(ModuleStoreTestCase):
             badge_helpers.filter_earned_badge_by_joined_team(self.user, self.course1, earned_badges=mock.ANY)
 
     def test_filter_earned_badge_by_joined_team_course_with_no_team(self):
+        """
+        For a course in which user has not joined any team, Assert that course team is None
+        and earned badge list is empty
+        :return: None
+        """
         course_team, earned_badges = badge_helpers.filter_earned_badge_by_joined_team(
-            self.user, self.course1, earned_badges=mock.ANY)
+        self.user, self.course1, earned_badges=mock.ANY)
 
         self.assertIsNone(course_team)
         self.assertEqual(earned_badges, list())
--- openedx/features/badging/tests/test_views.py
+++ openedx/features/badging/tests/test_views.py
@@ -24,7 +24,12 @@ class BadgeViewsTestCases(ModuleStoreTestCase):
         self.user = UserFactory()
 
     def test_trophycase_with_various_course(self):
-        request = self.request_factory.get(reverse('trophycase'))
+        """
+        Create 2 courses and their course enrollments (1 active & 1 inactive), without badges.
+        Assert that response code is 200 and expected json has data for only active course enrollment
+        :return: None
+        """
+        request = self.request_factory.get(reverse('trophycase'), {'json': True})
         request.user = self.user
 
         # create two courses here, one is already created via setUp
@@ -43,7 +48,14 @@ class BadgeViewsTestCases(ModuleStoreTestCase):
 
     @mock.patch('openedx.features.badging.views.populate_trophycase')
     def test_trophycase_with_some_earned_badges(self, mock_populate_trophycase):
-        request = self.request_factory.get(reverse('trophycase'))
+        """
+        Test trophy case for 2 badges, one badge earned by current user and both badges earned by some other user.
+        Assert that request is fetching data for only current logged in user for the badges only he has earned
+        in relevant course. And Badge earned by other user are ignored
+        :param mock_populate_trophycase: mock to assert it has been called with expected input
+        :return: None
+        """
+        request = self.request_factory.get(reverse('trophycase'), {'json': True})
         request.user = self.user
 
         badge1 = BadgeFactory()
@@ -61,14 +73,20 @@ class BadgeViewsTestCases(ModuleStoreTestCase):
         mock_populate_trophycase.assert_called_once_with(request.user, mock.ANY, [user_badge])
 
     def test_my_badges_denies_anonymous(self):
-        """This method test API call without logged-in user. In this case user must be redirected
-         to login page
+        """
+        This method test API call without logged-in user. In this case user must be redirected
+        to login page
+        :return: None
         """
         path = reverse('my_badges', kwargs={'course_id': 'course/123/1'})
         response = Client().get(path=path)
         self.assertRedirects(response, '{}?next={}'.format(reverse('signin_user'), path))
 
     def test_my_badges_invalid_course_id(self):
+        """
+        Test my badges with invalid course id. Assert that an error is raised
+        :return: None
+        """
         course_id = 'test/course/123'
         path = reverse('my_badges', kwargs={'course_id': course_id})
 
@@ -80,6 +98,11 @@ class BadgeViewsTestCases(ModuleStoreTestCase):
 
     @mock.patch('openedx.features.badging.views.get_course_badges')
     def test_my_badges_with_enrolled_and_active_course(self, mock_get_course_badges):
+        """
+        Test my badges with 1 active course enrollment for valid course
+        :param mock_get_course_badges: mock to assert it has been called with expected input
+        :return: None
+        """
         CourseEnrollmentFactory(user=self.user, course_id=self.course.id, is_active=True)
 
         path = reverse('my_badges', kwargs={'course_id': self.course.id})
@@ -91,6 +114,10 @@ class BadgeViewsTestCases(ModuleStoreTestCase):
         mock_get_course_badges.assert_called_once_with(request.user, self.course.id, list())
 
     def test_my_badges_with_enrolled_but_inactive_course(self):
+        """
+        Test my badges with inactive course. Assert that an error is raised
+        :return: None
+        """
         CourseEnrollmentFactory(user=self.user, course_id=self.course.id, is_active=False)
 
         path = reverse('my_badges', kwargs={'course_id': self.course.id})
@@ -102,6 +129,13 @@ class BadgeViewsTestCases(ModuleStoreTestCase):
 
     @mock.patch('openedx.features.badging.views.get_course_badges')
     def test_my_badges_with_some_earned_badges(self, mock_get_course_badges):
+        """
+        Test my badges for 2 badges, one badge earned by current user and both badges earned by some other user.
+        Assert that request is fetching data for only current logged in user for the badges only he has earned
+        in relevant course. And Badge earned by other user are ignored
+        :param mock_get_course_badges: mock to assert it has been called with expected input
+        :return: None
+        """
         CourseEnrollmentFactory(user=self.user, course_id=self.course.id, is_active=True)
 
         path = reverse('my_badges', kwargs={'course_id': self.course.id})
