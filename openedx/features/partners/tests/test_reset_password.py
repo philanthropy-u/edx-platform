2e57353a5fe7c0fb0558cd1fa5e336697f8a37aa
root <root@A002-00004.arbisoft.com>
2020-01-16 18:32:06 +0500

    Commit

--- openedx/features/partners/tests/test_reset_password.py
+++ openedx/features/partners/tests/test_reset_password.py
@@ -14,7 +14,7 @@ class ResetPasswordTestCases(APITestCase):
     def setUp(self):
         self.end_point = reverse('partner_reset_password')
 
-    def test_valid_partner_email_with_authenticated_user(self):
+    def test_valid_partner_email(self):
         self.user = UserFactory()
         self.client.force_authenticate(self.user)
 
@@ -25,6 +25,21 @@ class ResetPasswordTestCases(APITestCase):
         partner_1 = PartnerFactory(label="partner_1", main_logo='abc', small_logo='xyz', slug='partner_1')
 
         PartnerUserFactory(user_id=self.partner.id, partner_id=partner_1.id)
+
+        response = self.client.post(
+            self.end_point,
+            data=self.valid_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_valid_partner_email_not_affiliated_to_partner(self):
+        self.user = UserFactory()
+        self.client.force_authenticate(self.user)
+
+        self.valid_data = {
+            'email': 'abc@test.com'
+        }
+        self.partner = User.objects.create_user(username='testuser', password='12345',email='abc@test.com')
 
         response = self.client.post(
             self.end_point,
