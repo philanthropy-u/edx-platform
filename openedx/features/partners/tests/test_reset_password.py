a9cc0e803666fbd4037904cd5998ee7b968ac190
root <root@A002-00004.arbisoft.com>
2020-01-16 14:35:05 +0500

    Commit

--- openedx/features/partners/tests/test_reset_password.py
+++ openedx/features/partners/tests/test_reset_password.py
@@ -8,27 +8,21 @@ from rest_framework import status
 
 from openedx.features.partners.tests.factories import PartnerUserFactory, PartnerFactory
 
-
 from student.tests.factories import UserFactory
-
-
-
 
 class ResetPasswordTestCases(APITestCase):
     def setUp(self):
 
-        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
         self.end_point = reverse('partner_reset_password')
 
     def test_valid_partner_email_with_authenticated_user(self):
-        self.client.login(username='john', password='johnpassword')
-        self.partner = User.objects.create_user(username='testuser', password='12345', email='abc@test.com')
+        self.user = UserFactory()
+        self.client.force_authenticate(self.user)
 
         self.valid_data = {
             'email': 'abc@test.com'
         }
-
-
+        self.partner = User.objects.create_user(username='testuser', password='12345',email='abc@test.com')
         partner_1 = PartnerFactory(label="partner_1", main_logo='abc', small_logo='xyz', slug='partner_1')
 
         PartnerUserFactory(user_id=self.partner.id, partner_id=partner_1.id)
@@ -43,14 +37,10 @@ class ResetPasswordTestCases(APITestCase):
 
 
     def test_valid_partner_email_with_unauthenticated_user(self):
-        self.client.login(username='john', password='wrongpassword')
-        self.partner = User.objects.create_user(username='testuser', password='12345', email='abc@test.com')
-
         self.valid_data = {
             'email': 'abc@test.com'
         }
-
-
+        self.partner = User.objects.create_user(username='testuser', password='12345',email='abc@test.com')
         partner_1 = PartnerFactory(label="partner_1", main_logo='abc', small_logo='xyz', slug='partner_1')
 
         PartnerUserFactory(user_id=self.partner.id, partner_id=partner_1.id)
