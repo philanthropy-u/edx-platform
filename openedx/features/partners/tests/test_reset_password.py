0f0efb218e7782364193a7582a7d9800caf40363
root <root@A002-00004.arbisoft.com>
2020-01-16 14:14:20 +0500

    abcd

--- openedx/features/partners/tests/test_reset_password.py
+++ openedx/features/partners/tests/test_reset_password.py
@@ -16,33 +16,60 @@ from student.tests.factories import UserFactory
 
 class ResetPasswordTestCases(APITestCase):
     def setUp(self):
-        self.user = UserFactory()
-        self.client.force_authenticate(self.user)
-        self.abc='asfdsf'
-        self.partner = User.objects.create_user(username='testuser', password='12345',email='abc@test.com')
+
+        self.superuser = User.objects.create_superuser('john', 'john@snow.com', 'johnpassword')
         self.end_point = reverse('partner_reset_password')
+
+    def test_valid_partner_email_with_authenticated_user(self):
+        self.client.login(username='john', password='johnpassword')
+        self.partner = User.objects.create_user(username='testuser', password='12345', email='abc@test.com')
+
         self.valid_data = {
             'email': 'abc@test.com'
         }
-        self.invalid_data = {
-            'email': 'efg@test.com'
-        }
 
-        partner_1 = PartnerFactory(label="partner_1", main_logo='abc',small_logo='xyz',slug='partner_1')
+
+        partner_1 = PartnerFactory(label="partner_1", main_logo='abc', small_logo='xyz', slug='partner_1')
 
         PartnerUserFactory(user_id=self.partner.id, partner_id=partner_1.id)
 
-
-
-
-    def test_valid_partner_email(self):
         response = self.client.post(
             self.end_point,
             data=self.valid_data
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+
+
+
+    def test_valid_partner_email_with_unauthenticated_user(self):
+        self.client.login(username='john', password='wrongpassword')
+        self.partner = User.objects.create_user(username='testuser', password='12345', email='abc@test.com')
+
+        self.valid_data = {
+            'email': 'abc@test.com'
+        }
+
+
+        partner_1 = PartnerFactory(label="partner_1", main_logo='abc', small_logo='xyz', slug='partner_1')
+
+        PartnerUserFactory(user_id=self.partner.id, partner_id=partner_1.id)
+
+        response = self.client.post(
+            self.end_point,
+            data=self.valid_data
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+
+
+
     def test_invalid_partner_email(self):
+        self.invalid_data = {
+            'email': 'efg@test.com'
+        }
         response = self.client.post(
             self.end_point,
             data=self.invalid_data
