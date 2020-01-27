e45f38e4acd521719df7aa561b0599be52304d28
abdullah998 <52492005+abdullah998@users.noreply.github.com>
2020-01-27 19:10:55 +0500

    Rearranged imports

--- openedx/features/partners/tests/factories.py
+++ openedx/features/partners/tests/factories.py
@@ -1,12 +1,11 @@
 import factory
 from factory.django import DjangoModelFactory
 from faker.providers import internet
+from openedx.features.partners.models import Partner, PartnerUser, PartnerCommunity
 from student.tests.factories import UserFactory
 
 
 factory.Faker.add_provider(internet)
-
-from openedx.features.partners.models import Partner, PartnerUser, PartnerCommunity
 
 
 class PartnerFactory(DjangoModelFactory):
