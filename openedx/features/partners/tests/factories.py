c1137f4438d86c3e09e94efbd4521baadf6eeb52
root <root@A002-00004.arbisoft.com>
2020-02-03 20:11:35 +0500

    Added faker for slug and adjusted spaces

--- openedx/features/partners/tests/factories.py
+++ openedx/features/partners/tests/factories.py
@@ -4,18 +4,16 @@ from faker.providers import internet
 from openedx.features.partners.models import Partner, PartnerUser, PartnerCommunity
 from student.tests.factories import UserFactory
 
-
 factory.Faker.add_provider(internet)
-
 
 class PartnerFactory(DjangoModelFactory):
     class Meta(object):
         model = Partner
 
     label = factory.Faker('name')
+    slug = factory.Faker('slug')
     main_logo = 'dummy'
     small_logo = 'dummy'
-
 
 class PartnerUserFactory(DjangoModelFactory):
     class Meta(object):
