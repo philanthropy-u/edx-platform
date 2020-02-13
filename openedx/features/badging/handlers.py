8d3f5fe628dfe0f827f52db843a613504d9cb026
Zahid <zahid.ali@arbisoft.com>
2020-02-13 18:34:24 +0500

    LP-1602 Sending email when user earn badge

--- openedx/features/badging/handlers.py
+++ openedx/features/badging/handlers.py
@@ -1,12 +1,14 @@
-from django.dispatch import receiver
 from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
 
-from nodebb.tasks import task_sync_badge_info_with_nodebb, task_delete_badge_info_from_nodebb
+from nodebb.tasks import task_delete_badge_info_from_nodebb, task_sync_badge_info_with_nodebb
+
 from .models import Badge
 
 
 @receiver(post_save, sender=Badge)
 def sync_badge_info_with_nodebb(sender, instance, update_fields, **kwargs):
+    """When badge is created or updated in platform, sync it in NodeBB."""
     badge_info = {
         'id': instance.id,
         'name': instance.name,
@@ -19,8 +21,8 @@ def sync_badge_info_with_nodebb(sender, instance, update_fields, **kwargs):
 
 @receiver(post_delete, sender=Badge)
 def delete_badge_info_from_nodebb(sender, instance, **kwargs):
-
+    """On badge deletion, delete it from NodeBB. This will not effect any post count on NodeBB."""
     badge_data = {
-        "id": instance.id
+        'id': instance.id
     }
     task_delete_badge_info_from_nodebb.delay(badge_data)
