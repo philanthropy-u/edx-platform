5389432806a37a74544f4dd15404ea3b4e54e383
HassanArif <hassanarif@arbisofts-MacBook-Pro.local>
2020-02-18 15:30:30 +0500

    Add specializations about page url

--- openedx/features/specializations/urls.py
+++ openedx/features/specializations/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
-from .views import list_specializations
+from .views import list_specializations, specialization_about
 
 urlpatterns = [
     url(r'^specializations/$', list_specializations, name='list_specializations'),
+
+    # cd41d6fd068642738643a1c8bde8d360
+    url(r'^specializations/(?P<specialization_uuid>[0-9a-f]{32})$', specialization_about, name='specialization_about'),
 ]
