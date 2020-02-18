0c6d279063377e6a57916a79fde5af25180e0b4d
nofalfayyaz <fayyaznofal@gmail.com>
2020-02-18 13:50:38 +0500

    :sparkles: Philu newly earned badge notifications added

--- openedx/features/badging/handlers.py
+++ openedx/features/badging/handlers.py
@@ -1,8 +1,12 @@
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
+from edx_notifications.data import NotificationType
+from edx_notifications.lib.publisher import register_notification_type
+from edx_notifications.signals import perform_type_registrations
 from nodebb.tasks import task_delete_badge_info_from_nodebb, task_sync_badge_info_with_nodebb
 
+from .constants import EARNED_BADGE_NOTIFICATION_TYPE, JSON_NOTIFICATION_RENDERER
 from .models import Badge
 
 
@@ -26,3 +30,17 @@ def delete_badge_info_from_nodebb(sender, instance, **kwargs):
         'id': instance.id
     }
     task_delete_badge_info_from_nodebb.delay(badge_data)
+
+
+@receiver(perform_type_registrations)
+def register_notification_types(sender, **kwargs):  # pylint: disable=unused-argument
+    """
+    Register philu NotificationTypes.
+    This will be called automatically on the Notification subsystem startup
+    """
+    register_notification_type(
+            NotificationType(
+                name=EARNED_BADGE_NOTIFICATION_TYPE,
+                renderer=JSON_NOTIFICATION_RENDERER,
+            )
+    )
