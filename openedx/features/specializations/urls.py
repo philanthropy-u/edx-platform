d91735f97ef940dc228745c3e086ee5a2789aafb
HassanArif <hassanarif@arbisofts-MacBook-Pro.local>
2020-02-19 18:35:43 +0500

    feedback changes

--- openedx/features/specializations/urls.py
+++ openedx/features/specializations/urls.py
@@ -4,7 +4,5 @@ from .views import list_specializations, specialization_about
 
 urlpatterns = [
     url(r'^specializations/$', list_specializations, name='list_specializations'),
-
-    # cd41d6fd068642738643a1c8bde8d360
     url(r'^specializations/(?P<specialization_uuid>[0-9a-f]{32})$', specialization_about, name='specialization_about'),
 ]
