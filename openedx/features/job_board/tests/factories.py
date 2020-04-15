13945f06e982845e8c28cf785e1aa49954693397
AbdullahShahid <abdullah.shahid@arbisoft.com>
2020-04-15 19:18:46 +0500

    Implemented unit tests for job board

--- openedx/features/job_board/tests/factories.py
+++ openedx/features/job_board/tests/factories.py
@@ -1,5 +1,4 @@
-import factory
-from factory import fuzzy
+import factory.fuzzy
 
 from openedx.features.job_board.constants import JOB_COMPENSATION_CHOICES, JOB_HOURS_CHOICES, JOB_TYPE_CHOICES
 from openedx.features.job_board.models import Job
@@ -7,14 +6,14 @@ from openedx.features.job_board.models import Job
 
 class JobFactory(factory.django.DjangoModelFactory):
 
-    class Meta():
+    class Meta:
         model = Job
 
     title = factory.Faker('pystr', min_chars=1, max_chars=255)
     company = factory.Faker('pystr', min_chars=1, max_chars=255)
-    type = fuzzy.FuzzyChoice(JOB_TYPE_CHOICES)
-    compensation = fuzzy.FuzzyChoice(JOB_COMPENSATION_CHOICES)
-    hours = fuzzy.FuzzyChoice(JOB_HOURS_CHOICES)
+    type = factory.fuzzy.FuzzyChoice(JOB_TYPE_CHOICES)
+    compensation = factory.fuzzy.FuzzyChoice(JOB_COMPENSATION_CHOICES)
+    hours = factory.fuzzy.FuzzyChoice(JOB_HOURS_CHOICES)
     city = 'Lahore'
     country = 'PK'
     description = factory.Faker('pystr', min_chars=1, max_chars=255)
