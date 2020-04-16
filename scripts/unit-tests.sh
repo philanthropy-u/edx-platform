26d2b0429bb8acfc5dd76c69d73cf80f77301757
Muhammad Ali <muhammadali@A003-00280.arbisoft.com>
2020-04-17 01:11:20 +0500

    unit tests added

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -55,15 +55,15 @@ case "${TEST_SUITE}" in
     "lms-unit")
         case "$SHARD" in
             "all")
-                paver test_system -s lms ${PAVER_ARGS} ${PARALLEL} 
+                paver test_system -s lms ${PAVER_ARGS} ${PARALLEL} 2> lms-tests.log
                 mv reports/.coverage reports/.coverage.lms
                 ;;
             [1-9])
-                paver test_system -s lms --eval-attr="shard==$SHARD" ${PAVER_ARGS} ${PARALLEL} 
+                paver test_system -s lms --eval-attr="shard==$SHARD" ${PAVER_ARGS} ${PARALLEL} 2> lms-tests.${SHARD}.log
                 mv reports/.coverage reports/.coverage.lms.${SHARD}
                 ;;
             10|"noshard")
-                paver test_system -s lms --eval-attr="shard>=$SHARD or not shard" ${PAVER_ARGS} ${PARALLEL} 
+                paver test_system -s lms --eval-attr="shard>=$SHARD or not shard" ${PAVER_ARGS} ${PARALLEL} 2> lms-tests.10.log
                 mv reports/.coverage reports/.coverage.lms.10
                 ;;
             *)
