b182f3111c7a6c99de0f2508f2e99e01d065ec14
qasimgulzar <qasim.gulzar@arbisoft.com>
2020-04-29 12:42:15 +0500

    LP-1970 OTHER Implement Marketplace Request Form [CHANGE] restrict user to change organization in marketplace request form.

--- openedx/features/marketplace/decorators.py
+++ openedx/features/marketplace/decorators.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseForbidden
 
 def has_affiliated_user(function=None):
     def inner(request, *args, **kwargs):
-        if request.user.extended_profile.organization:
+        if request.user.is_authenticated and request.user.extended_profile.organization:
             return function(request, *args, **kwargs)
         return HttpResponseForbidden('Access denied!')
 
