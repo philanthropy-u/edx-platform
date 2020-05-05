26a153827ae6609b20e554c35e63f00a20c261c8
Abdullah Zafar <mabdullahzafar08@gmail.com>
2020-05-05 11:39:44 +0500

    Refactor unit tests to use locally created objects

--- openedx/features/job_board/tests/views/test_job_board.py
+++ openedx/features/job_board/tests/views/test_job_board.py
@@ -1,3 +1,7 @@
+from ddt import data, ddt, unpack
+from rest_framework import status
+from w3lib.url import add_or_replace_parameters
+
 from django.test import TestCase
 from django.urls import reverse
 
@@ -19,18 +23,13 @@ from openedx.features.job_board.constants import (
 from openedx.features.job_board.models import Job
 from openedx.features.job_board.tests.factories import JobFactory
 from openedx.features.job_board.views import JobCreateView, JobListView
-from rest_framework import status
 
 
+@ddt
 class JobBoardViewTest(TestCase):
 
     def setUp(self):
-        self.page_length = 10
-        self.number_of_jobs = 15
         self.client.logout()
-        self.jobs = []
-        for i in range(self.number_of_jobs):
-            self.jobs.append(JobFactory())
 
     @classmethod
     def setUpClass(cls):
@@ -56,7 +55,7 @@ class JobBoardViewTest(TestCase):
                                            "logo": "",
                                            "type": "remote",
                                            "website_link": ""})
-        self.assertEqual(Job.objects.all().count(), self.number_of_jobs + 1)
+        self.assertEqual(Job.objects.all().count(), 1)
 
     def test_create_view_post_unsuccessful(self):
         self.client.post('/jobs/create/', {"function": "dummy function",
@@ -72,154 +71,129 @@ class JobBoardViewTest(TestCase):
                                            "logo": "",
                                            "type": "remote",
                                            "website_link": ""})
-        self.assertEqual(Job.objects.all().count(), self.number_of_jobs)
+        self.assertEqual(Job.objects.all().count(), 0)
 
     def test_job_detail_view_invalid_pk(self):
         response = self.client.get(reverse('job_detail', kwargs={'pk': 1}), follow=True)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_job_detail_view_valid_pk(self):
-        response = self.client.get(reverse('job_detail', kwargs={'pk': self.jobs[0].pk}), follow=True)
+        job = JobFactory()
+        response = self.client.get(reverse('job_detail', kwargs={'pk': job.id}), follow=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_job_list_view(self):
         response = self.client.get(reverse('job_list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(JobListView.paginate_by, self.page_length)
+        self.assertEqual(JobListView.paginate_by, 10)
         self.assertEqual(JobListView.ordering, ['-created'])
 
-    def test_job_list_view_pagination(self):
-        response_first_page = self.client.get(reverse('job_list'))
-        self.assertEqual(response_first_page.status_code, status.HTTP_200_OK)
-        self.assertTrue('is_paginated' in response_first_page.context_data)
-        self.assertTrue(response_first_page.context_data['is_paginated'] is True)
-        self.assertTrue(len(response_first_page.context_data['job_list']) == self.page_length)
-
-        response_second_page = self.client.get(reverse('job_list') + '?page=2')
+    @unpack
+    @data(('', 10), ('?page=2', 4))
+    def test_job_list_view_pagination(self, page_query_param, job_list_size):
+        # Create multiple new jobs to test pagination.
+        jobs = JobFactory.create_batch(14)
+        response_second_page = self.client.get(reverse('job_list') + page_query_param)
         self.assertEqual(response_second_page.status_code, status.HTTP_200_OK)
         self.assertTrue('is_paginated' in response_second_page.context_data)
         self.assertTrue(response_second_page.context_data['is_paginated'] is True)
-        self.assertTrue(len(response_second_page.context_data['job_list']) == self.number_of_jobs-self.page_length)
+        self.assertTrue(len(response_second_page.context_data['job_list']) == job_list_size)
 
-    def test_job_list_view_filters_job_type(self):
-        number_of_remote_jobs = 0
-        number_of_onsite_jobs = 0
+    @data(JOB_TYPE_REMOTE_KEY, JOB_TYPE_ONSITE_KEY)
+    def test_job_list_view_filters_job_type(self, job_type):
+        # Create a new job with `type=job_type` to search for.
+        job = JobFactory(type=job_type)
 
-        for job in self.jobs:
-            if job.type is JOB_TYPE_REMOTE_KEY:
-                number_of_remote_jobs += 1
-            if job.type is JOB_TYPE_ONSITE_KEY:
-                number_of_onsite_jobs += 1
+        query_params = {job_type: JOB_PARAM_TRUE_VALUE}
+        response = self.client.get(add_or_replace_parameters(reverse('job_list'), query_params))
 
-        response = self.client.get(reverse('job_list') + '?' + JOB_TYPE_REMOTE_KEY + '=' + JOB_PARAM_TRUE_VALUE)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(response.context_data['search_fields'][JOB_TYPE_REMOTE_KEY], JOB_PARAM_TRUE_VALUE)
-        self.assertTrue(response.context_data['filtered'], True)
-        self.assertTrue(len(response.context_data['job_list']) == number_of_remote_jobs)
-        for job in response.context_data['job_list']:
-            self.assertEqual(job.type, JOB_TYPE_REMOTE_KEY)
+        self.assertTrue(response.context_data['search_fields'][job_type], JOB_PARAM_TRUE_VALUE)
 
-        response = self.client.get(reverse('job_list') + '?' + JOB_TYPE_ONSITE_KEY + '=' + JOB_PARAM_TRUE_VALUE)
+        # ensure that no other filter was applied
+        del [response.context_data['search_fields'][job_type]]
+        self.assertTrue(not any(response.context_data['search_fields'].values()))
+
+        self.assertTrue(response.context_data['filtered'], True)
+        self.assertTrue(len(response.context_data['job_list']) == 1)
+        self.assertEqual(response.context_data['job_list'].first().type, job_type)
+        self.assertEqual(response.context_data['job_list'].first().pk, job.pk)
+
+    @data(JOB_COMP_VOLUNTEER_KEY, JOB_COMP_HOURLY_KEY, JOB_COMP_SALARIED_KEY)
+    def test_job_list_view_filters_job_compensation(self, job_comp):
+        # Create a new job with `compensation=job_comp` to search for.
+        job = JobFactory(compensation=job_comp)
+
+        query_params = {job_comp: JOB_PARAM_TRUE_VALUE}
+        response = self.client.get(add_or_replace_parameters(reverse('job_list'), query_params))
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(response.context_data['search_fields'][JOB_TYPE_ONSITE_KEY], JOB_PARAM_TRUE_VALUE)
+        self.assertTrue(response.context_data['search_fields'][job_comp], JOB_PARAM_TRUE_VALUE)
+
+        # ensure that no other filter was applied
+        del [response.context_data['search_fields'][job_comp]]
+        self.assertTrue(not any(response.context_data['search_fields'].values()))
+
         self.assertTrue(response.context_data['filtered'], True)
-        self.assertTrue(len(response.context_data['job_list']) == number_of_onsite_jobs)
-        for job in response.context_data['job_list']:
-            self.assertEqual(job.type, JOB_TYPE_ONSITE_KEY)
+        self.assertTrue(len(response.context_data['job_list']) == 1)
+        self.assertEqual(response.context_data['job_list'].first().compensation, job_comp)
+        self.assertEqual(response.context_data['job_list'].first().pk, job.pk)
 
-    def test_job_list_view_filters_job_compensation(self):
-        number_of_volunteer_jobs = 0
-        number_of_hourly_jobs = 0
-        number_of_salaried_jobs = 0
+    @data(JOB_HOURS_FULLTIME_KEY, JOB_HOURS_PARTTIME_KEY, JOB_HOURS_FREELANCE_KEY)
+    def test_job_list_view_filters_job_hours(self, job_hours):
+        # Create a new job with `hours=job_hours` to search for.
+        job = JobFactory(hours=job_hours)
 
-        for job in self.jobs:
-            if job.compensation is JOB_COMP_VOLUNTEER_KEY:
-                number_of_volunteer_jobs += 1
-            if job.compensation is JOB_COMP_HOURLY_KEY:
-                number_of_hourly_jobs += 1
-            if job.compensation is JOB_COMP_SALARIED_KEY:
-                number_of_salaried_jobs += 1
+        query_params = {job_hours: JOB_PARAM_TRUE_VALUE}
+        response = self.client.get(add_or_replace_parameters(reverse('job_list'), query_params))
 
-        response = self.client.get(reverse('job_list') + '?' + JOB_COMP_VOLUNTEER_KEY + '=' + JOB_PARAM_TRUE_VALUE)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(response.context_data['search_fields'][JOB_COMP_VOLUNTEER_KEY], JOB_PARAM_TRUE_VALUE)
+        self.assertTrue(response.context_data['search_fields'][job_hours], JOB_PARAM_TRUE_VALUE)
+
+        # ensure that no other filter was applied
+        del [response.context_data['search_fields'][job_hours]]
+        self.assertTrue(not any(response.context_data['search_fields'].values()))
+
         self.assertTrue(response.context_data['filtered'], True)
-        self.assertTrue(len(response.context_data['job_list']) == number_of_volunteer_jobs)
-        for job in response.context_data['job_list']:
-            self.assertEqual(job.compensation, JOB_COMP_VOLUNTEER_KEY)
-
-        response = self.client.get(reverse('job_list') + '?' + JOB_COMP_HOURLY_KEY + '=' + JOB_PARAM_TRUE_VALUE)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(response.context_data['search_fields'][JOB_COMP_HOURLY_KEY], JOB_PARAM_TRUE_VALUE)
-        self.assertTrue(response.context_data['filtered'], True)
-        self.assertTrue(len(response.context_data['job_list']) == number_of_hourly_jobs)
-        for job in response.context_data['job_list']:
-            self.assertEqual(job.compensation, JOB_COMP_HOURLY_KEY)
-
-        response = self.client.get(reverse('job_list') + '?' + JOB_COMP_SALARIED_KEY + '=' + JOB_PARAM_TRUE_VALUE)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(response.context_data['search_fields'][JOB_COMP_SALARIED_KEY], JOB_PARAM_TRUE_VALUE)
-        self.assertTrue(response.context_data['filtered'], True)
-        self.assertTrue(len(response.context_data['job_list']) == number_of_salaried_jobs)
-        for job in response.context_data['job_list']:
-            self.assertEqual(job.compensation, JOB_COMP_SALARIED_KEY)
-
-    def test_job_list_view_filters_job_hours(self):
-        number_of_fulltime_jobs = 0
-        number_of_parttime_jobs = 0
-        number_of_freelance_jobs = 0
-
-        for job in self.jobs:
-            if job.hours is JOB_HOURS_FULLTIME_KEY:
-                number_of_fulltime_jobs += 1
-            if job.hours is JOB_HOURS_PARTTIME_KEY:
-                number_of_parttime_jobs += 1
-            if job.hours is JOB_HOURS_FREELANCE_KEY:
-                number_of_freelance_jobs += 1
-
-        response = self.client.get(reverse('job_list') + '?' + JOB_HOURS_FULLTIME_KEY + '=' + JOB_PARAM_TRUE_VALUE)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(response.context_data['search_fields'][JOB_HOURS_FULLTIME_KEY], JOB_PARAM_TRUE_VALUE)
-        self.assertTrue(response.context_data['filtered'], True)
-        self.assertTrue(len(response.context_data['job_list']) == number_of_fulltime_jobs)
-        for job in response.context_data['job_list']:
-            self.assertEqual(job.hours, JOB_HOURS_FULLTIME_KEY)
-
-        response = self.client.get(reverse('job_list') + '?' + JOB_HOURS_PARTTIME_KEY + '=' + JOB_PARAM_TRUE_VALUE)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(response.context_data['search_fields'][JOB_HOURS_PARTTIME_KEY], JOB_PARAM_TRUE_VALUE)
-        self.assertTrue(response.context_data['filtered'], True)
-        self.assertTrue(len(response.context_data['job_list']) == number_of_parttime_jobs)
-        for job in response.context_data['job_list']:
-            self.assertEqual(job.hours, JOB_HOURS_PARTTIME_KEY)
-
-        response = self.client.get(reverse('job_list') + '?' + JOB_HOURS_FREELANCE_KEY + '=' + JOB_PARAM_TRUE_VALUE)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(response.context_data['search_fields'][JOB_HOURS_FREELANCE_KEY], JOB_PARAM_TRUE_VALUE)
-        self.assertTrue(response.context_data['filtered'], True)
-        self.assertTrue(len(response.context_data['job_list']) == number_of_freelance_jobs)
-        for job in response.context_data['job_list']:
-            self.assertEqual(job.hours, JOB_HOURS_FREELANCE_KEY)
+        self.assertTrue(len(response.context_data['job_list']) == 1)
+        self.assertEqual(response.context_data['job_list'].first().hours, job_hours)
+        self.assertEqual(response.context_data['job_list'].first().pk, job.pk)
 
     def test_job_list_view_filters_job_location(self):
+        # Create a new job with custom location to search for.
         job = JobFactory(country='PK', city='Karachi')
-        response = self.client.get(reverse(
-            'job_list') + '?' + JOB_PARAM_CITY_KEY + '=' + job.city + '&' + JOB_PARAM_COUNTRY_KEY + '=' + job.country.name)
+
+        query_params = {JOB_PARAM_CITY_KEY: job.city, JOB_PARAM_COUNTRY_KEY: job.country.name}
+        response = self.client.get(add_or_replace_parameters(reverse('job_list'), query_params))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(response.context_data['search_fields']['country'], job.country.name)
         self.assertTrue(response.context_data['search_fields']['city'], job.city)
+
+        # ensure that no other filter was applied
+        del [response.context_data['search_fields']['country']]
+        del [response.context_data['search_fields']['city']]
+        self.assertTrue(not any(response.context_data['search_fields'].values()))
+
         self.assertTrue(response.context_data['filtered'], True)
         self.assertTrue(len(response.context_data['job_list']) == 1)
         self.assertTrue(response.context_data['job_list'][0].country.name == job.country.name)
         self.assertTrue(response.context_data['job_list'][0].city == job.city)
 
     def test_job_list_view_filters_job_query(self):
+        # Create a new job with custom title to search for.
         job = JobFactory(title='custom_job')
-        response = self.client.get(reverse('job_list') + '?' + JOB_PARAM_QUERY_KEY + '=' + job.title)
+
+        query_params = {JOB_PARAM_QUERY_KEY: job.title}
+        response = self.client.get(add_or_replace_parameters(reverse('job_list'), query_params))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue(response.context_data['search_fields']['query'], job.title)
+
+        # ensure that no other filter was applied
+        del [response.context_data['search_fields']['query']]
+        self.assertTrue(not any(response.context_data['search_fields'].values()))
+
         self.assertTrue(response.context_data['filtered'], True)
         self.assertTrue(len(response.context_data['job_list']) == 1)
         self.assertTrue(job.title in response.context_data['job_list'][0].title)
