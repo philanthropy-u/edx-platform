be59fe534ed8437bbdf7b9b282f01cd9c6d5d204
Abdullah998 <abdullah.shahid@arbisoft.com>
2020-05-08 02:04:36 +0500

    LP-1855 User Community Mapping

--- openedx/features/classrooms/models.py
+++ openedx/features/classrooms/models.py
@@ -6,7 +6,7 @@ from model_utils.models import TimeStampedModel
 
 class DiscussionCommunityMembership(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    community_url = models.ForeignKey(DiscussionCommunity, on_delete=models.CASCADE)
+    community = models.ForeignKey(DiscussionCommunity, on_delete=models.CASCADE)
 
     class Meta:
-        unique_together = (("user", "community_url"),)
+        unique_together = (("user", "community"),)
